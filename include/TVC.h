e15ae2b3e5e72c29036ace94563c5a6c0f0f89f0
Charley griffin <boingr66@gmail.com>
2022-09-08 23:55:42 -0500

    Changed the centering positions for each servo

--- include/TVC.h
+++ include/TVC.h
@@ -9,30 +9,30 @@ void TVCInitalize()
     servox.attach(3);
     servoy.attach(5);
     servoy.write(55);
-    servox.write(45);
+    servox.write(55);
     delay(125);
-    servox.write(85);
+    servox.write(95);
     delay(125);
-    servox.write(45);
+    servox.write(55);
     delay(125);
     servoy.write(95);
     delay(125);
     servoy.write(55);
     delay(125);
-    servox.write(5);
+    servox.write(15);
     delay(125);
-    servox.write(45);
+    servox.write(55);
     delay(125);
     servoy.write(15);
     delay(500);
-    servox.write(5);
+    servox.write(15);
     delay(125);
-    servox.write(45);
+    servox.write(55);
     servoy.write(55);
 }
 
 void TVCCenter()
 {
-    servox.write(45);
+    servox.write(55);
     servoy.write(55);
 }
